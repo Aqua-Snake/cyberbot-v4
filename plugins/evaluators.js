9ead419dde35794e4b88a6e5f5463587b0a99d45
Aqua Eagle <91973695+Aqua-Eagle@users.noreply.github.com>
2021-10-29 05:25:39 -0700

    Add files via upload

--- plugins/evaluators.js
+++ plugins/evaluators.js
@@ -155,7 +155,7 @@ antlch()
 var ldc = ''
 if (Config.LANG == 'AZ') ldc = '*Bağlantı Aşkarlandı!*'
 if (Config.LANG == 'TR') ldc = '*‎Link Tespit Edildi!*'
-if (Config.LANG == 'EN') ldc = '*Link Detected!*'
+if (Config.LANG == 'EN') ldc = '*Link Detected!* \n ```Delete it before you remove.```'
 if (Config.LANG == 'ML') ldc = '*ലിങ്ക് കണ്ടെത്തി!*'
 if (Config.LANG == 'ID') ldc = '*Tautan Terdeteksi!*'
 if (Config.LANG == 'PT') ldc = '*Link Detectado!*'
@@ -171,25 +171,43 @@ Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (mess
             var im = await checkImAdmin(message)
             if (!im) return;
             if (us) return;
+            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data });
+            await message.client.sendMessage(message.jid,'5', MessageType.text, {quoted: message.data });
+            await message.client.sendMessage(message.jid,'4', MessageType.text, {quoted: message.data });
+            await message.client.sendMessage(message.jid,'3', MessageType.text, {quoted: message.data });
+            await message.client.sendMessage(message.jid,'2', MessageType.text, {quoted: message.data });
+            await message.client.sendMessage(message.jid,'1', MessageType.text, {quoted: message.data });
             await message.client.groupRemove(message.jid, [message.data.participant]);         
-            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
+            await message.client.sendMessage(message.jid,'Removed', MessageType.text, {quoted: message.data })
         } 
         else if (regex2.test(message.message)) {
             var us = await checkUsAdmin(message)
             var im = await checkImAdmin(message)
             if (!im) return;
             if (us) return;
+            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data });
+            await message.client.sendMessage(message.jid,'5', MessageType.text, {quoted: message.data });
+            await message.client.sendMessage(message.jid,'4', MessageType.text, {quoted: message.data });
+            await message.client.sendMessage(message.jid,'3', MessageType.text, {quoted: message.data });
+            await message.client.sendMessage(message.jid,'2', MessageType.text, {quoted: message.data });
+            await message.client.sendMessage(message.jid,'1', MessageType.text, {quoted: message.data });
             await message.client.groupRemove(message.jid, [message.data.participant]);         
-            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
-        }
+            await message.client.sendMessage(message.jid,'Removed', MessageType.text, {quoted: message.data })
+        } 
         else if (message.message.match(/((?:[.]com)\b)/i)) {
             var us = await checkUsAdmin(message)
             var im = await checkImAdmin(message)
             if (!im) return;
             if (us) return;
+            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data });
+            await message.client.sendMessage(message.jid,'5', MessageType.text, {quoted: message.data });
+            await message.client.sendMessage(message.jid,'4', MessageType.text, {quoted: message.data });
+            await message.client.sendMessage(message.jid,'3', MessageType.text, {quoted: message.data });
+            await message.client.sendMessage(message.jid,'2', MessageType.text, {quoted: message.data });
+            await message.client.sendMessage(message.jid,'1', MessageType.text, {quoted: message.data });
             await message.client.groupRemove(message.jid, [message.data.participant]);         
-            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
-        }
+            await message.client.sendMessage(message.jid,'Removed', MessageType.text, {quoted: message.data })
+        } 
     }
 }));
 Asena.addCommand({pattern: 'term ?(.*)', fromMe: true, desc: Lang.TERM_DESC}, (async (message, match) => {    
